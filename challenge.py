94ee98ffa66f0775cf23a93ce1e1c981c566fe3f
Jonathan Calabrese <57088280+Yonibrese@users.noreply.github.com>
2019-11-17 15:29:53 +0200

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,12 @@
+first_name = input( "Please type your first name.")
+last_name = input("please type your last name.")
+
+print(f"Hello {first_name} {last_name}!!!!")
+age = input("how old are you?")
+home_town = input("where are you from?")
+print(f"you are {age} years old, and you are from {home_town}")
+
+last_job = input("where was the last place you worked?")
+last_job_duration = input("How long did you work there?")
+
+print(f"you last job was at {last_job}, and you worked there for {last_job_duration}.")
